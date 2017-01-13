3220a689c1ba6d891eb1cafca05e7e779269c514
Artem Gilmudinov <guliash92@gmail.com>
2017-01-13 10:09:52 +0300

    form tests; wip;

--- quizzes/users/test_forms.py
+++ quizzes/users/test_forms.py
@@ -6,3 +6,17 @@ class SignupFormTestCase(TestCase):
     def test_empty_name__not_valid(self):
         data = {'name':'', 'email': 'test@example.com', 'password': 'test'}
         self.assertFalse(SignupForm(data).is_valid())
+
+    def test_empty_email__not_valid(self):
+        data = {'name': 'test', 'email': '', 'password': 'test'}
+        self.assertFalse(SignupForm(data).is_valid())
+
+    def test_empty_password__not_valid(self):
+        data = {'name': 'test', 'email': 'test@example.com', 'password': ''}
+        self.assertFalse(SignupForm(data).is_valid())
+
+    def test_bad_email__not_valid(self):
+        data = {'name': 'test', 'email': '@example.com', 'password': 'test'}
+        form = SignupForm(data)
+        self.assertFalse(form.is_valid())
+        self.assertTrue(form.has_error('email', 'invalid'))
